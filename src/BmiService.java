4ee138312c8df52349ff66a1d6d37c169299b9de
Anton <toni-popov93@mail.ru>
2022-05-28 20:41:49 +0300

    BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,7 @@
 public class BmiService {
-    public double calculate(double mass) {
-        int veight = 71;
+    public double calculate(int veight) {
         double growth = 1.78;
-
-        double bodyMassIndex = 71 / (1.78 * 1.78);
+        double bodyMassIndex = veight / (growth * growth);
 
         return bodyMassIndex;
     }
