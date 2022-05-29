b52969d70f9b2b33c8c9f2f677762b30c5a675f0
Anton <toni-popov93@mail.ru>
2022-05-29 10:03:52 +0300

    Changes

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public double calculate(int veight) {
-        double growth = 1.78;
+    public double calculate(int veight, double growth) {
         double bodyMassIndex = veight / (growth * growth);
 
         return bodyMassIndex;
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
     BmiService service = new BmiService();
     int veight = 71;
-    double bodyMassIndex = service.calculate(veight);
+    double growth = 1.78;
+    double bodyMassIndex = service.calculate(veight, growth);
 
     System.out.println(bodyMassIndex);
     }
